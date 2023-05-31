596206f528f3b698944b9c953dd07781342fdf0d
Enzo Supicciatti <esupicciattiutn@gmail.com>
2023-05-31 10:27:11 -0300

    se agrega codigo para guardar user en local storage desde index

--- js/index.js
+++ js/index.js
@@ -130,3 +130,26 @@ username: "Enzo Supicciatti"; */
     myAccount.href = "/pages/login.html";
   }
 });
+
+// Obtener los usuarios almacenados en el localStorage
+let usersJSON = localStorage.getItem("users");
+
+if (usersJSON) {
+  // Convertir la cadena JSON a un array de objetos
+  users = JSON.parse(usersJSON);
+} else {
+  users = [
+    {
+      email: "admin@admin.com",
+      password: "Admin1234",
+      username: "admin",
+      avatar: "../assets/photos/cuteBear.jpg",
+    },
+  ];
+  saveUsersToLocalStorage();
+}
+// Función para guardar los usuarios en el localStorage
+function saveUsersToLocalStorage() {
+  let usersJSON = JSON.stringify(users);
+  localStorage.setItem("users", usersJSON);
+}
